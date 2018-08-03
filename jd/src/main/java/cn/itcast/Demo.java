818ecbbe8ce6db0691ca70a41bb3f309090f5bb3
hubin <ithubin@163.com>
2018-08-03 18:31:54 +0800

    demo commitss

--- jd/src/main/java/cn/itcast/Demo.java
+++ jd/src/main/java/cn/itcast/Demo.java
@@ -8,8 +8,9 @@ public class Demo {
     public static void main(String[] args) {
 
         System.out.println("这是a程序员修改");
-
         System.out.println("这是B程序员修改的代码");
+
+        System.out.println("A程序员开发代码了，大家小心了..");
     }
 
 }
